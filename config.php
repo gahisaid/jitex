acff0bf86a1b3b3c27294726a23ed150f34201ef
gahi said <said20avril@gmail.com>
2026-03-05 22:37:32 +0000

    JA-2 Créer le fichier config.php pour établir la connexion avec la base de données

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php 
+
+// Etablir la connexion à la base données en utilisant PDO ou MYSQLI
+
+
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php 
+
+// récupérer les données provenant de index.html
+
+$username=$_POST["username"];
+$password=md5($_POST["password"]);
+
+// Vérification si l'utilisateur existe dans la abse de données
+
